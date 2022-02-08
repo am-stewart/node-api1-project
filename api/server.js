d92ed41549d4841117e2379cfca86a742bb74702
Allison Stewart <amstewart@yahoo.com>
2022-02-07 21:03:25 -0500

    set up post request

--- api/server.js
+++ api/server.js
@@ -5,5 +5,31 @@ const server = express();
 
 server.use(express.json());
 
+server.get('/', (req, res) => {
+  console.log('received get request');
+  res.json("hello world!");
+});
+
+server.post('/api/users', (req, res) => {
+  let { body } = req;
+
+  if(!body.name || !body.bio) {
+    res.status(400).json({ message: "Please provide name and bio for the user" });
+  } else {
+    model.insert(body)
+    .then(user => {
+      res.status(201).json(user);
+    })
+    .catch(() => {
+      res.status(500).json({ message: 'could not create user' });
+    });
+  }
+});
+
+
+
+
+
+
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = require('./api/server');
 const port = 9000;
 
 // START YOUR SERVER HERE
-console.log('hello world')
+console.log('hello world!!!')
 
 server.listen(port, () => {
   console.log('server started');
